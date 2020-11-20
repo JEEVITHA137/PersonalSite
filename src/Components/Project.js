233236b86248ff64a1c2cdf78459ee7357070188
JEEVITHA137 <47686236+JEEVITHA137@users.noreply.github.com>
2020-11-20 17:19:45 +0530

    Add files via upload

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -11,7 +11,7 @@ function Projects() {
         </div>
         
         <div className="row d-flex pt-5">
-        <img className="w-100 col-sm-7 pt-3" src="./ConferenceSite.png" alt=""></img>
+        <img className="w-100 col-sm-7 pt-3" src="./MiniProject.png" alt=""></img>
         <p className="col-sm-5"><h3>Mini Project</h3>To Retrieve the IOT data from the IOT Device,Stored it in the cloud and access it through the Website.It is created to analyze the IOT lab experiments data.
         <br/><a className="btn" href="https://github.com/JEEVITHA137/Mini_Project_" target="_blank" rel="noopener noreferrer">Repository Link </a></p>
         </div>
@@ -38,7 +38,7 @@ function Projects() {
         <div className ="col-sm-5 shadow-lg p-3 m-4 bg-white rounded">
         <h3 className="text-align-center">Building Escape</h3>
         <video width="320" height="240" controls>
-        <source src="./SimpleShooter.mp4" type="video/mp4"/>
+        <source src="./BuildingEscape.mp4" type="video/mp4"/>
         </video>
         <p>A <span>Building Escape Game</span> created using Unreal Engine 4. This game is focused on the environment creation and some physics. The aim of the game is to <span>find the statue, stolen it and thrown away</span>.</p>
         </div>
@@ -48,7 +48,7 @@ function Projects() {
         <div className ="col-sm-5 shadow-lg p-3 m-4 bg-white rounded">
         <h3 className="text-align-center">Toon Tanks</h3>
         <video width="320" height="240" controls>
-        <source src="./SimpleShooter.mp4" type="video/mp4"/>
+        <source src="./ToonTank.mp4" type="video/mp4"/>
         </video>
         <p>A <span>Toon Tank Game</span> created using Unreal Engine 4. This game is focused on the physics based movements and particle effect. The Aim of the game is to <span>shoot all the enemy tanks</span>.</p>
         </div>
@@ -56,7 +56,7 @@ function Projects() {
         <div className ="col-sm-5 shadow-lg p-3 m-4 bg-white rounded">
         <h3 className="text-align-center">BullCow Game</h3>
         <video width="320" height="240" controls>
-        <source src="./SimpleShooter.mp4" type="video/mp4"/>
+        <source src="./BullCowGame.mp4" type="video/mp4"/>
         </video>
         <p>A <span>BullCow Game</span> created using Unreal Engine 4. This game is focued on some basics(Unreal c++) like constructors, BeginPlay, Inputs from player, conditions and loops. The aim of the game is to find the <span>Isogram word</span>(A Word with non repeating letters) by <span>using Bull and Cow count</span>.</p>
         </div>
