63499fbab5b707723243d6b82028dcc368b23132
Jeevitha Venkatesan <jeevitha.venkatesan@surveysparrow.com>
2022-05-27 23:26:10 +0530

    adding experience

---
+++ src/Components/Experience.js
@@ -0,0 +1,13 @@
+
+import './component.css';
+
+function Experience() {
+  return (
+    <div className="experience container justify-content-center">
+      <div className="title">Experience</div>
+      <p>1 year 2 months as a Product Developer at SurveySparrow, currently debugging and fixing real time customer issues in the Survey Platform powered with React, Node, Postgres and Sequelize.</p>
+    </div>
+  );
+}
+
+export default Experience;
